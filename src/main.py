97f9e5b37e1b76e6dcb7630612dbef108446233c
Irene Iriarte-Carretero <irene@irenes-air.lan>
2022-02-06 11:26:37 +0000

    Changes feb 2022

--- src/main.py
+++ src/main.py
@@ -1,133 +1,24 @@
-import mwapi
-from mwapi.errors import APIError
-from mwviews.api import PageviewsClient
+import nlp
+import wikidata
+import mwapi_queries
 
-import requests
 import pandas as pd
 import numpy as np
 
 import datetime
 
 
-def image_usage_query(image):
-    session = mwapi.Session(host='https://en.wikipedia.org/',
-                            user_agent='measuring_impact/0.0 (irene.iriarte.c@gmail.com)')
-
-    continued = session.get(
-        formatversion=2,
-        action='query',
-        generator='imageusage',
-        giutitle=image,
-        giulimit=100,  # 100 results per request
-        continuation=True)
-
-    image_usage = []
-    for portion in continued:
-        if 'query' in portion:
-            for page in portion['query']['pages']:
-                image_usage.append(page['title'])
-
-    print("Image {} appears {} times".format(image, len(image_usage)))
-
-    if len(image_usage) == 0:
-        image_usage = ['Image not used']
-
-    return image_usage
-
-
-def images_owned_by():
-    url = 'https://query.wikidata.org/sparql'
-
-    query = """
-    SELECT DISTINCT *
-    WHERE {
-        ?collection wdt:P127 wd:Q666063.
-        ?item wdt:P195 ?collection.
-        ?item wdt:P18 ?image.
-        BIND(CONCAT("File:", STRAFTER(wikibase:decodeUri(STR(?image)), "http://commons.wikimedia.org/wiki/Special:FilePath/")) AS ?fileTitle)
-      }
-    LIMIT 300
-    """
-
-    r = requests.get(url, params={'format': 'json',
-                                  'query': query})
-
-    data = r.json()
-
-    images = []
-    for item in data['results']['bindings']:
-        images.append(item['fileTitle']['value'])
-
-    print("Fetched {} images".format(len(images)))
-
-    return images
-
-
-def images_in_collection():
-    url = 'https://query.wikidata.org/sparql'
-
-    query = """
-    SELECT DISTINCT *
-    WHERE {
-        ?item wdt:P195 wd:Q23817605.
-        ?item wdt:P18 ?image.
-        BIND(CONCAT("File:", STRAFTER(wikibase:decodeUri(STR(?image)), "http://commons.wikimedia.org/wiki/Special:FilePath/")) AS ?fileTitle)
-      }
-    LIMIT 100
-    """
-
-    r = requests.get(url, params={'format': 'json',
-                                  'query': query})
-
-    data = r.json()
-
-    images = []
-    for item in data['results']['bindings']:
-        images.append(item['fileTitle']['value'])
-
-    print("Fetched {} images".format(len(images)))
-
-    return images
-
-
-def collections_owned_by():
-    url = 'https://query.wikidata.org/sparql'
-
-    query = """
-    SELECT DISTINCT ?collectionLabel
-    WHERE {
-        ?collection wdt:P127 wd:Q666063.
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
-      }
-    """
-
-    r = requests.get(url, params={'format': 'json',
-                                  'query': query})
-
-    data = r.json()
-
-
-def page_views_query(page):
-    p = PageviewsClient(user_agent="measuring_impact/0.0 (irene.iriarte.c@gmail.com)")
-
-    views = p.article_views('en.wikipedia', 
-                            page, 
-                            granularity='monthly', 
-                            start='20200101', 
-                            end='20201001')
-    
-    return views
-
-
 def pipeline():
-    images = images_owned_by()
+    images = wikidata.images_owned_by()
     
     # image_usage = {}
     # for image in images:
-    #     image_usage[image] = image_usage_query(image)
+    #     image_usage[image] = mwapi_queries.image_usage_query(image)
 
     import json
     image_usage = json.load(open('image_usage.json','r'))
+
+    create_collection_main_words(images=images)
 
     images = []
     pages = []
@@ -143,51 +34,7 @@ def pipeline():
     page_views = {}
     for image, pages in image_usage.items():
         if pages != ['Image not used']:
-            page_views[image] =  page_views_query(pages)
-
-    page_view_keys = [k  for  k in  page_views.keys()]
-    dates = [k for k in page_views[page_view_keys[0]].keys()]
-
-    dates_data = {}
-    for date in np.sort(dates):
-        dates_data[date] = []
-        for entry in image_usage_df.iterrows():
-            if entry[1][1] != 'Image not used':
-                dates_data[date].append(page_views[entry[1][0]][date][entry[1][1].replace(' ','_')])
-            else:
-                dates_data[date].append(None)
-        image_usage_df[date] = dates_data[date]
-
-    image_usage_agg = image_usage_df.groupby('image').sum()
-
-    return image_usage_df
-
-
-if __name__ == '__main__':
-    images = images_owned_by()
-    
-    # image_usage = {}
-    # for image in images:
-    #     image_usage[image] = image_usage_query(image)
-
-    import json
-    image_usage = json.load(open('image_usage.json','r'))
-
-    images = []
-    pages = []
-    for image, page in image_usage.items():
-        images.append(image)
-        pages.append(page)
-
-    image_usage_df = pd.DataFrame()
-    image_usage_df['image'] = images
-    image_usage_df['pages'] = pages
-    image_usage_df = image_usage_df.explode('pages')
-
-    page_views = {}
-    for image, pages in image_usage.items():
-        if pages != ['Image not used']:
-            page_views[image] =  page_views_query(pages)
+            page_views[image] =  mwapi_queries.page_views_query(pages)
 
     page_view_keys = [k  for  k in  page_views.keys()]
     dates = [k for k in page_views[page_view_keys[0]].keys()]
@@ -206,5 +53,61 @@ if __name__ == '__main__':
 
     import pdb
     pdb.set_trace()
+    return image_usage_df
+
+
+if __name__ == '__main__':
+    images = wikidata.images_in_collection()
+    import pdb
+    pdb.set_trace()
+    # import json
+    # image_usage = json.load(open('image_usage.json','r'))
+
+    image_usage = {}
+    for image in images:
+        image_usage[image] = mwapi_queries.image_usage_query(image)
+
+    image_corpus = {}
+    final_results = pd.DataFrame()
+
+    for i, image in enumerate(images):
+        print("***********************************************")
+        print("Processing image {}, {} out of {}".format(image, i+1, len(images)))
+        final_words = nlp.create_image_main_words(image_title=image,
+                                                  nlp_filter='landscape')
+        search_results = mwapi_queries.word_search_query(words=final_words,
+                                                         image_title=image)
+        image_corpus[image] = mwapi_queries.entry_text_query(pages=search_results)
+        if len(search_results) > 0:
+            tf_idf = nlp.tf_idf(page_summaries=image_corpus[image],
+                                final_words=final_words,
+                                image=image)
+
+            tf_idf.loc[:,'page_views'] = np.tile(None, len(tf_idf))
+            tf_idf.loc[:,'page_completeness'] = np.tile(None, len(tf_idf))
+
+            views = {}
+            for page in search_results:
+                page_views = mwapi_queries.page_views_query(page)
+                tf_idf.loc[tf_idf.entry==page,'page_views'] = int(page_views)
+                tf_idf.loc[tf_idf.entry==page,'page_completeness'] = mwapi_queries.page_completeness(page=page)
+                similar_images = mwapi_queries.image_uniqueness(page=page,main_image=image,relevant_words=final_words)
+                tf_idf.loc[tf_idf.entry==page, 'image_uniqueness'] = np.tile(similar_images, len(tf_idf[tf_idf.entry == page]))
+
+        else:
+            tf_idf = None
+        final_results = final_results.append(tf_idf)
+
+    final_results['page_views_percentile'] = pd.qcut(final_results['page_views'].values, 100, labels=False, duplicates='drop')
+    final_results['page_completeness_percentile'] = pd.qcut(final_results['page_completeness'].values, 100, labels=False, duplicates='drop')
+
+    final_results['final_score'] = 0.25*final_results['word_relevance'] \
+                                 + 0.25*final_results['page_views_percentile']*0.01 \
+                                 + 0.25*final_results['page_completeness_percentile']*0.01 \
+                                 + 0.25*final_results['image_uniqueness']
+    final_results.final_score = final_results.final_score.round(2)
+    final_results = final_results.reset_index(drop=True)
+
+    final_results.to_csv('final_results.csv',index=False)
 
 
