e7c7129d00fadeeb8c0681a75c2ec30b1708229a
Irene Iriarte-Carretero <irene@irenes-air.lan>
2022-02-06 11:26:05 +0000

    Changes feb 2022

--- src/app.py
+++ src/app.py
@@ -1,53 +1,85 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
+import wikipedia
+
 import os
 
 import main
 
-st.title('Wikipedia Images User Visits')
+st.title('Collection Images Suggestions')
 
-@st.cache
-def load_data():
-	image_usage_df = main.pipeline()
-	return image_usage_df
-
-def get_image(filename):
-	import urllib
-	parsed_filename = urllib.parse.quote(filename).replace(' ', '_')
-	urllib.request.urlretrieve("http://commons.wikimedia.org/wiki/Special:Redirect/file/"+parsed_filename, os.path.join('images/','file_'+filename))
-
-	return os.path.join('images/','file_'+filename)
-
-data_load_state = st.text('Loading data...')
-image_usage_df = load_data()
-data_load_state.text('Loading data...done!')
-
-st.subheader('Raw data')
-st.write(image_usage_df)
-
-selected_indices = st.multiselect('Select images:', image_usage_df.image.unique())
-st.write(selected_indices)
-selected_rows = pd.DataFrame(image_usage_df[image_usage_df.image.isin(selected_indices)])
-st.write(selected_rows)
-
-for image in selected_indices:
-	filepath = get_image(image)
-	image_obj = Image.open(filepath)
-	st.image(image_obj, width=500, caption=image)
+sidebar_text = st.sidebar.text(
+    'Displaying results of model'
+)
 
 
-selected_pages = st.multiselect('Select pages:', selected_rows.pages.unique())
-st.write(selected_pages)
-selected_page_rows = pd.DataFrame(selected_rows[selected_rows.pages.isin(selected_pages)])
+weight1 = st.sidebar.slider(
+	'What weight would you like to place on word relevance?',
+	0.00,1.00,0.01
+)
 
-selected_page_rows = selected_page_rows.reset_index(drop=True)
-new_index = []
-for row in selected_page_rows.iterrows(): 
-	new_index.append(row[1][0] +"|"+ row[1][1])
-selected_page_rows.index = new_index
+weight2 = st.sidebar.slider(
+	'What weight would you like to place on entry views?',
+	0.00,1.00,0.01
+)
 
-st.subheader('Chart of image visits')
-linedata = selected_page_rows[selected_page_rows.columns.values].T[3:-1]
-st.line_chart(linedata)
+weight3 = st.sidebar.slider(
+	'What weight would you like to place on entry completeness?',
+	0.00,1.00,0.01
+)
+
+weight4 = st.sidebar.slider(
+	'What weight would you like to place on image uniqueness?',
+	0.00,1.00,0.01
+)
+
+# eq_weighting = st.sidebar.button('Click here for equal weighting')
+# if eq_weighting:
+# 	weight1 = 0.25
+# 	weight2 = 0.25
+# 	weight3 = 0.25
+# 	weight4 = 0.25
+
+
+confirmation_button = st.sidebar.button('Confirm weights and rerun')
+
+# suggested_results = pd.read_csv('final_results_25072021.csv')
+# suggested_results = pd.read_csv('results_final_pilot_coll_compound_250721.csv')
+# suggested_results = pd.read_csv('final_results_11082021.csv')
+suggested_results = pd.read_csv('final_results.csv')
+
+if confirmation_button:
+	suggested_results['final_score'] = weight1*suggested_results['word_relevance'] \
+									 + weight2*suggested_results['page_views_percentile']*0.01 \
+									 + weight3*suggested_results['page_completeness_percentile']*0.01 \
+									 + weight4*suggested_results['image_uniqueness']
+
+st.write('Current weights:', weight1, weight2, weight3, weight4)
+selected_indices = st.multiselect('Select images:', suggested_results.image.unique())
+selected_rows = pd.DataFrame(suggested_results[suggested_results.image.isin(selected_indices)])
+if len(selected_indices)>0:
+	st.dataframe(data=selected_rows, width=1024, height=768)
+	slider_max = selected_rows.shape[0]
+else:
+	selected_rows = suggested_results
+	slider_max = suggested_results.shape[0]
+
+if len(selected_rows) > 10:
+	slider_num = st.slider("Select number of top suggestions from dataset", 1, min(slider_max,100))
+else:
+	slider_num = 0
+
+selected_rows = selected_rows.sort_values(by='final_score', ascending=False)
+selected_rows = selected_rows.reset_index(drop=True)
+
+for i, result in selected_rows.head(max(10,slider_num)).iterrows():
+	st.subheader('SUGGESTION {}'.format(i+1))
+	st.write('Image', result['image'], 'could be placed in Wikipedia entry', result['entry'], 'with a total score of', round(result['final_score'],2))
+	st.write('It shows a word relevancy of', round(result['word_relevance'],2), 'for the following words:', result['relevant_words'])
+	st.write('The page has had an average of', result['page_views'], 'monthly views since the start of 2020 which is higher than', result['page_views_percentile'], 'percent of other entries.' )
+	st.write('It currently has approximately', result['page_completeness'], 'words which is higher than', result['page_completeness_percentile'], 'percent of other entries.')
+	st.write('It has an image uniqueness of', result['image_uniqueness'])
+	with st.beta_expander("See Wikipedia summary of entry"):
+	    st.write(wikipedia.summary(result['entry']))
+	
