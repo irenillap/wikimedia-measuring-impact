1e062e4e1b8787a368c6d6a5a7bd049736664c70
Irene Iriarte-Carretero <irene@irenes-air.lan>
2022-03-20 08:32:41 +0000

    Use NER

--- src/nlp.py
+++ src/nlp.py
@@ -9,189 +9,201 @@ from flair.data import Sentence
 
 
 def create_image_main_words(image_title, nlp_filter=None, ner_filter = None, tagger = None, output_type = ['PER','LOC','ORG','MISC']):
-        """
+    """
 
-	Function to extract key words from the relevant image title. To do this, remove all digits,
-	punctuation and stopwords from image title. Also uses an nlp filter if there is one to place
-	more emphasis on words relevant to image collection (eg 'castle' for landscape images)
-	
-	Inputs:
-	* image_title - string of image title as appears on Wikimedia
-	* nlp_filter - string to call on manually created nlp filter if needed
-	* ner_filter - whether to use a ner filter. if use, must specify a tagger
-	* tagger - a ner model
-	* output_type - type of named entity to keep. possible values are ['PER','LOC','ORG','MISC'], where PER = person name; LOC = location name; ORG = organization name; MISC = other name. default to ['PER','LOC','ORG','MISC']
+    Function to extract key words from the relevant image title. To do this, remove all digits,
+    punctuation and stopwords from image title. Also uses an nlp filter if there is one to place
+    more emphasis on words relevant to image collection (eg 'castle' for landscape images)
     
-	Outputs:
-	* final_words - set of final relevant words extracted from title
-	
-	"""
-        image_title_words = []
+    Inputs:
+    * image_title - string of image title as appears on Wikimedia
+    * nlp_filter - string to call on manually created nlp filter if needed
+    * ner_filter - whether to use a ner filter. if use, must specify a tagger
+    * tagger - a ner model
+    * output_type - type of named entity to keep. possible values are ['PER','LOC','ORG','MISC'], where PER = person name; LOC = location name; ORG = organization name; MISC = other name. default to ['PER','LOC','ORG','MISC']
+    
+    Outputs:
+    * final_words - set of final relevant words extracted from title
+    
+    """
+    image_title_words = []
+    remove_digits = str.maketrans('', '', string.digits)
+    remove_punctuation = str.maketrans('','',string.punctuation)
+
+    if image_title[-4:] == 'jpeg':
+        image_words = image_title[5:-4].translate(remove_digits)
+    else:
+        image_words = image_title[5:-3].translate(remove_digits)
+
+    final_words = []
+
+    image_words = image_words.translate(remove_punctuation)
+    image_title_words = image_words.split()
+    image_title_words = [word.lower() for word in image_title_words]
+
+    if nlp_filter:
+        final_words = apply_nlp_filter(words=image_title_words, 
+                                       nlp_filter=nlp_filter)
+    else:
+     	final_words = image_title_words
+
+    stopwords_eng = set(stopwords.words('english'))
+
+    if ner_filter:
+        if tagger == None:
+            raise Exception("must specify a tagger to use ner filter")
+        ner_results = apply_ner_filter(image_words, tagger = tagger, output_type = output_type)
+        final_words.append(ner_results)
+
+    final_words = [word for word in final_words if not word in stopwords_eng]
+    final_words = set(final_words)
+    
+    return final_words
+
+
+def preprocess_summaries(page_summaries):
+    """
+    Function to create full corpus of words from candidate pages summaries
+
+    Inputs:
+    * page_summaries - dictionary of image being studied and summary text of all 
+    candidate Wikipedia entries
+
+    Outputs:
+    * all_summaries - list of all words in all relevant summaries
+    """
+    all_summaries = []
+
+    for image, summary in page_summaries.items():
         remove_digits = str.maketrans('', '', string.digits)
         remove_punctuation = str.maketrans('','',string.punctuation)
 
-        if image_title[-4:] == 'jpeg':
-                image_words = image_title[5:-4].translate(remove_digits)
-        else:
-                image_words = image_title[5:-3].translate(remove_digits)
+        new_summary = summary.translate(remove_digits)
+        new_summary = new_summary.translate(remove_punctuation)
+
+        summary_words = new_summary.split()
+        summary_words = [word.lower() for word in summary_words]
         
-        if ner_filter:
-            if tagger == None:
-                raise Exception("must specify a tagger to use ner filter")
-            image_title_words = apply_ner_filter(image_words, tagger = tagger, output_type = output_type)
-        
-        image_words = image_words.translate(remove_punctuation)
-        image_title_words = image_words.split()
-        image_title_words = [word.lower() for word in image_title_words]
+        all_summaries.append(summary_words)
 
-        if nlp_filter:
-                final_words = apply_nlp_filter(words=image_title_words, 
-                                                           nlp_filter=nlp_filter)
-
-        stopwords_eng = set(stopwords.words('english'))
-
-        final_words = [word for word in final_words if not word in stopwords_eng]
-        final_words = set(final_words)
-
-        return final_words
-
-def preprocess_summaries(page_summaries):
-	"""
-	Function to create full corpus of words from candidate pages summaries
-
-	Inputs:
-	* page_summaries - dictionary of image being studied and summary text of all 
-	candidate Wikipedia entries
-
-	Outputs:
-	* all_summaries - list of all words in all relevant summaries
-	"""
-	all_summaries = []
-
-	for image, summary in page_summaries.items():
-		remove_digits = str.maketrans('', '', string.digits)
-		remove_punctuation = str.maketrans('','',string.punctuation)
-
-		new_summary = summary.translate(remove_digits)
-		new_summary = new_summary.translate(remove_punctuation)
-
-		summary_words = new_summary.split()
-		summary_words = [word.lower() for word in summary_words]
-		
-		all_summaries.append(summary_words)
-
-	return all_summaries
+    return all_summaries
 
 
 def tf_idf(page_summaries, training_summaries, final_words, image):
-	"""
-	Function to calculate tf-idf score defining word relevance for image and candidate Wikipedia entry
+    """
+    Function to calculate tf-idf score defining word relevance for image and candidate Wikipedia entry
 
-	Inputs:
-	* page_summaries - dictionary of image being studied and summary text of all 
-	candidate Wikipedia entries
-	* training_summaries - dictionary of all benchmark summaries
-	* final_words - set of relevant words extracted from image
-	* image - string of image title as appears on Wikimedia
+    Inputs:
+    * page_summaries - dictionary of image being studied and summary text of all 
+    candidate Wikipedia entries
+    * training_summaries - dictionary of all benchmark summaries
+    * final_words - set of relevant words extracted from image
+    * image - string of image title as appears on Wikimedia
 
-	Outputs:
-	* relevant_df - pd DataFrame of numerical relevance of relevant words
-	"""
-	all_summaries = []
-	all_pages = []
-	pages = []
+    Outputs:
+    * relevant_df - pd DataFrame of numerical relevance of relevant words
+    """
+    all_summaries = []
+    all_pages = []
+    pages = []
 
-	all_corpus = page_summaries.copy()   # start with keys and values of x
-	all_corpus.update(training_summaries)
+    all_corpus = page_summaries.copy()   # start with keys and values of x
+    all_corpus.update(training_summaries)
 
-	for page, summary in all_corpus.items():
-		all_summaries.append(summary)
-		all_pages.append(page)
+    for page, summary in all_corpus.items():
+        all_summaries.append(summary)
+        all_pages.append(page)
 
-	for page, summary in page_summaries.items():
-		pages.append(page)
-	
-	vectorizer = TfidfVectorizer()
-	vectors = vectorizer.fit_transform(all_summaries)
-	feature_names = vectorizer.get_feature_names()
-	dense = vectors.todense()
-	denselist = dense.tolist()
+    for page, summary in page_summaries.items():
+        pages.append(page)
+    
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform(all_summaries)
+    feature_names = vectorizer.get_feature_names()
+    dense = vectors.todense()
+    denselist = dense.tolist()
 
-	df = pd.DataFrame(denselist, columns=feature_names)
+    df = pd.DataFrame(denselist, columns=feature_names)
 
-	relevant_words = [word for word in final_words if word in df.columns.values]
-	relevant_df = df[relevant_words].copy()
-	relevant_df.loc[:,'word_relevance'] = relevant_df.sum(axis=1).values
+    relevant_words = [word for word in final_words if word in df.columns.values]
+    relevant_df = df[relevant_words].copy()
+    relevant_df.loc[:,'word_relevance'] = relevant_df.sum(axis=1).values
 
-	relevant_df.loc[:,'image'] = image
+    relevant_df.loc[:,'image'] = image
 
-	ordered_indexes = list(relevant_df.sort_values(by='word_relevance', ascending=False).index.values)
+    ordered_indexes = list(relevant_df.sort_values(by='word_relevance', ascending=False).index.values)
 
-	print("RESULTS FOR IMAGE {}:".format(image))
-	print("Relevant words are {}".format(relevant_words))
-	ordered_pages = []
+    print("RESULTS FOR IMAGE {}:".format(image))
+    print("Relevant words are {}".format(relevant_words))
+    ordered_pages = []
 
-	relevant_df.loc[:, 'entry'] = all_pages
+    relevant_df.loc[:, 'entry'] = all_pages
 
-	relevant_df.loc[:,'relevant_words'] = pd.Series([relevant_words for i in relevant_df.index],dtype = 'object')
-	relevant_df = relevant_df[relevant_df['word_relevance'] > 0]
-	relevant_df = relevant_df.sort_values(by='word_relevance', ascending=False)
-	relevant_df = relevant_df[['image','entry','word_relevance', 'relevant_words']]
-	relevant_df = relevant_df[relevant_df.entry.isin(pages)]
+    relevant_df.loc[:,'relevant_words'] = pd.Series([relevant_words for i in relevant_df.index],dtype = 'object')
+    relevant_df = relevant_df[relevant_df['word_relevance'] > 0]
+    relevant_df = relevant_df.sort_values(by='word_relevance', ascending=False)
+    relevant_df = relevant_df[['image','entry','word_relevance', 'relevant_words']]
+    relevant_df = relevant_df[relevant_df.entry.isin(pages)]
 
-	return relevant_df
+    return relevant_df
 
 
 def apply_nlp_filter(words, nlp_filter):
-	"""
-	Function to apply nlp filter if required. Applying the filter creates compound relevant words 
-	e.g. if 'Harlem Castle from East' is title, normal words would pick up ['Harlem', 'Castle', 'East']. This 
-	function will also append ['Harlem Castle'] to the list.
+    """
+    Function to apply nlp filter if required. Applying the filter creates compound relevant words 
+    e.g. if 'Harlem Castle from East' is title, normal words would pick up ['Harlem', 'Castle', 'East']. This 
+    function will also append ['Harlem Castle'] to the list.
 
-	Inputs:
-	* words - set of relevant words extracted from an image title 
-	* nlp_filter - string of applied nlp filter
+    Inputs:
+    * words - set of relevant words extracted from an image title 
+    * nlp_filter - string of applied nlp filter
 
-	Outputs:
-	* new_words - updated set of relevant words for image title
-	"""
-	if nlp_filter == 'landscape':
-		keywords = ['castle', 'river', 'abbey', 'hill', 'shire', 'baths', 'bridge', 'church', 'waterfall','mountain', 'vale']
+    Outputs:
+    * new_words - updated set of relevant words for image title
+    """
+    if nlp_filter == 'landscape':
+        keywords = ['castle', 'river', 'abbey', 'hill', 'shire', 'baths', 'bridge', 'church', 'waterfall','mountain', 'vale']
 
-	new_words = words.copy()
-	for i, word in enumerate(words):
-		if word in keywords:
-			if len(words) > i+1 and words[i+1] == 'of':
-				new_word = word + " " + words[i+1] + " " + words[i+2]
-			else:
-				new_word = words[i-1] + " " + word
-			new_words.append(new_word)
-			print("Appending {} to words".format(new_word))
+        new_words = words.copy()
+        for i, word in enumerate(words):
+            if word in keywords:
+                if len(words) > i+1 and words[i+1] == 'of':
+                    new_word = word + " " + words[i+1] + " " + words[i+2]
+                else:
+                    new_word = words[i-1] + " " + word
+                new_words.append(new_word)
+                print("Appending {} to words".format(new_word))
 
-	return new_words
-	
+    elif nlp_filter == 'portraits':
+        new_words = words.copy()
+        if words[0] == 'portrait':
+            new_word =  ' '.join(words[2:])
+            new_words.append(new_word)
+
+    return new_words
+    
 def apply_ner_filter(words, tagger, output_type):
   
-  """
-  Function to apply a named entity recognition filter to get rid of irrelevant words
+    """
+    Function to apply a named entity recognition filter to get rid of irrelevant words
 
-  Input:
-  * words (image_title) - string of image title as appears on Wikimedia
-  * tagger - a ner model
-  * output_type - type of named entity to keep. possible values are ['PER','LOC','ORG','MISC'], where PER = person name; LOC = location name; ORG = organization name; MISC = other name
+    Input:
+    * words (image_title) - string of image title as appears on Wikimedia
+    * tagger - a ner model
+    * output_type - type of named entity to keep. possible values are ['PER','LOC','ORG','MISC'], where PER = person name; LOC = location name; ORG = organization name; MISC = other name
 
-  Output:
-  * filtered_words - updated set of named entity words for image title
+    Output:
+    * filtered_words - updated set of named entity words for image title
 
-  example:
-  ner_filter('George Washington went to Washington',['PER'],SequenceTagger.load("flair/ner-english-fast"))
-  -> 'George Washington'
-  """
+    example:
+    ner_filter('George Washington went to Washington',['PER'],SequenceTagger.load("flair/ner-english-fast"))
+    -> 'George Washington'
+    """
 
-  sentence = Sentence(words)
+    sentence = Sentence(words)
 
-  # predict NER tags
-  tagger.predict(sentence)
-
-  return ' '.join([entity.text for entity in sentence.get_spans('ner') if entity.tag in output_type])
+    # predict NER tags
+    tagger.predict(sentence)
+    print(' '.join([entity.text for entity in sentence.get_spans('ner') if entity.tag in output_type]))
+    return ' '.join([entity.text for entity in sentence.get_spans('ner') if entity.tag in output_type])
 
