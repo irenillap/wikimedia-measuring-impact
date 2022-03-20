256425a40c531e2eeae221b5bf7a31b55481d413
Irene Iriarte-Carretero <irene@irenes-air.lan>
2022-03-20 08:32:28 +0000

    Order by asc item for consistency

--- src/wikidata.py
+++ src/wikidata.py
@@ -77,6 +77,50 @@ def images_in_collection(collection_wikipedia_id, retrieval_limit=25):
     return images
 
 
+def images_in_portrait_collection():
+    """
+    Function to return 200 images on Wikimedia tagged as belonging to a certain collection
+
+    Input:
+    * collection_wikipedia_id: string of collection id from wikimedia
+    * retrieval_limit: int of limit of retrieval
+
+    Output:
+    * images: dictionary
+    """
+    url = 'https://query.wikidata.org/sparql'
+
+
+    query = """
+    SELECT DISTINCT ?item ?image ?fileTitle ?sitter ?sitterLabel
+    WHERE {
+        ?item wdt:P195 wd:Q54859927.
+        ?item wdt:P18 ?image.
+        BIND(CONCAT("File:", STRAFTER(wikibase:decodeUri(STR(?image)), "http://commons.wikimedia.org/wiki/Special:FilePath/")) AS ?fileTitle)
+        ?item wdt:P921 ?sitter.
+        
+              
+        SERVICE wikibase:label {
+       bd:serviceParam wikibase:language "en" 
+    }}
+     ORDER BY ASC(?item)
+     LIMIT 50
+    """
+
+    r = requests.get(url, params={'format': 'json',
+                                  'query': query})
+
+    data = r.json()
+
+    images = []
+    for item in data['results']['bindings']:
+        images.append(item['fileTitle']['value'])
+
+    print("Fetched {} images".format(len(images)))
+
+    return images
+
+
 def process_pilot_collection():
     """
     Function to return and process 200 images on Wikimedia tagged as belonging to the NLW landscape collection
